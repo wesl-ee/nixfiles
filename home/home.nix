4ce37b852178e12dbb67af36c457b41e9ff9feeb
Wesley Coakley <w@wesleycoakley.com>
2022-07-22 02:35:12 -0400

    firefox common-settings for minimizing telemetry

--- home/home.nix
+++ home/home.nix
@@ -103,7 +103,50 @@
   services.password-store-sync = {
   };
 
-  programs.firefox = {
+  programs.firefox = let common-settings = {
+      "browser.startup.homepage" = "https://wesl.ee/";
+      "app.shield.optoutstudies.enabled" = false;
+      "browser.contentblocking.category" = "standard";
+      "browser.download.autohideButton" = false;
+      "browser.formfill.enable" = false;
+      "browser.newtabpage.enabled" = false;
+      "browser.newtabpage.activity-stream.asrouter.userprefs.cfr.addons" = false;
+      "browser.newtabpage.activity-stream.asrouter.userprefs.cfr.features" = false;
+      "browser.newtabpage.activity-stream.feeds.section.topstories" = false;
+      "browser.newtabpage.activity-stream.feeds.topsites" = false;
+      "browser.newtabpage.activity-stream.showSearch" = false;
+      "browser.safebrowsing.malware.enabled" = false;
+      "browser.safebrowsing.phishing.enabled" = false;
+      "browser.search.region" = "US";
+      "browser.search.suggest.enabled" = false;
+      "browser.urlbar.suggest.history" = false;
+      "browser.urlbar.suggest.quicksuggest.nonsponsored" = false;
+      "browser.urlbar.suggest.quicksuggest.sponsored" = false;
+      "browser.urlbar.suggest.searches" = false;
+      "browser.urlbar.suggest.topsites" = false;
+      "datareporting.healthreport.uploadEnabled" = false;
+      "dom.security.https_only_mode" = true;
+      "dom.security.https_only_mode_ever_enabled" = true;
+      "extensions.formautofill.addresses.enabled" = false;
+      "extensions.formautofill.creditCards.enabled" = false;
+      "extensions.pictureinpicture.enable_picture_in_picture_overrides" = true;
+      "extensions.ui.dictionary.hidden" = true;
+      "extensions.ui.locale.hidden" = true;
+      "extensions.ui.sitepermission.hidden" = true;
+      "layout.spellcheckDefault" = 0;
+      "network.dns.disablePrefetch" = true;
+      "network.predictor.enabled" = false;
+      "network.prefetch-next" = false;
+      "places.history.enabled" = false;
+      "privacy.donottrackheader.enabled" = true;
+      "privacy.history.custom" = true;
+      "privacy.userContext.enabled" = true;
+      "trailhead.firstrun.didSeeAboutWelcome" = true;
+      "toolkit.telemetry.reportingpolicy.firstRun" = false;
+      "toolkit.telemetry.pioneer-new-studies-available" = false;
+      "signon.rememberSignons" = false;
+      "services.sync.engine.history" = false;
+    }; in {
     enable = true;
     extensions = with pkgs.nur.repos.rycee.firefox-addons; [
       noscript
@@ -115,22 +158,21 @@
     ];
     profiles = {
       browse = {
-	    isDefault = true;
 	    id = 0;
         bookmarks = {
           "Home Manager Config Options" = {
             url = "https://rycee.gitlab.io/home-manager/options.html";
           };
 	    };
-	    settings = {
-	      "browser.startup.homepage" = "https://wesl.ee/";
-	    };
+        settings = pkgs.lib.recursiveUpdate common-settings {
+        };
       };
       levana = {
         id = 1;
-        isDefault = false;
         bookmarks = {
 	    };
+        settings = pkgs.lib.recursiveUpdate common-settings {
+        };
       };
     };
   };
