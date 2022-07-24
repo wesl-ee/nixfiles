3212bcc77228243abe9f5f3ea6a783fce4b0d728
Wesley Coakley <w@wesleycoakley.com>
2022-07-24 05:52:44 -0400

    r-r-r-r-r-redshift

--- hosts/wonder-pop/configuration.nix
+++ hosts/wonder-pop/configuration.nix
@@ -22,6 +22,19 @@
   # Set your time zone.
   time.timeZone = "US/Eastern";
 
+  location.provider = "geoclue2";
+  services.redshift = {
+    enable = true;
+    brightness = {
+      day = "1";
+      night = "1";
+    };
+    temperature = {
+      day = 5500;
+      night = 3700;
+    };
+  };
+
   # Enable the X11 windowing system.
   services.xserver = {
     enable = true;
