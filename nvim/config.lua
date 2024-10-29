bed2b73fe1b2b11c969312747c6bd76ec61586ba
Wesley Coakley <w@wesl.ee>
2024-10-29 11:06:43 -0400

    fix deprecated gitsigns hl

--- nvim/config.lua
+++ nvim/config.lua
@@ -33,11 +33,11 @@ vim.keymap.set('i', '<C-n>', require('cmp').mapping.complete(), { noremap=true, 
 -- Plugin setup
 require('gitsigns').setup({
   signs = {
-    add          = {hl = 'GitSignsAdd'   , text = '+', numhl='GitSignsAddNr'   , linehl='GitSignsAddLn'},
-    change       = {hl = 'GitSignsChange', text = '~', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn'},
-    delete       = {hl = 'GitSignsDelete', text = '-', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn'},
-    topdelete    = {hl = 'GitSignsDelete', text = '-', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn'},
-    changedelete = {hl = 'GitSignsChange', text = '~', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn'},
+    add          = {text = '+'},
+    change       = {text = '~'},
+    delete       = {text = '-'},
+    topdelete    = {text = '-'},
+    changedelete = {text = '~'},
   },
 })
 require("sessions").setup({
